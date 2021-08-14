78368422a21a8c54fd826542c4f79804418dc8de
Ayman Wageah <aymn.wageah@gmail.com>
2021-08-14 20:48:51 +0200

    # implementing Singleton Design Pattern

--- Design Patterns/src/main/java/com/journaldev/creational/singleton/EagerInitializedSingleton.java
+++ Design Patterns/src/main/java/com/journaldev/creational/singleton/EagerInitializedSingleton.java
@@ -2,4 +2,14 @@ package com.journaldev.creational.singleton;
 
 public class EagerInitializedSingleton {
 
+    private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
+
+    //private constructor to avoid client applications to use constructor
+    private EagerInitializedSingleton() {
+    }
+
+    public static EagerInitializedSingleton getInstance() {
+        return instance;
+    }
+
 }
---
+++ Design Patterns/src/main/java/com/journaldev/creational/singleton/StaticBlockSingleton.java
@@ -0,0 +1,24 @@
+package com.journaldev.creational.singleton;
+
+public class StaticBlockSingleton {
+
+    private static final StaticBlockSingleton instance;
+
+    private StaticBlockSingleton() {
+
+    }
+
+    //static block initialization for exception handling
+    static {
+        try {
+            instance = new StaticBlockSingleton();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception occured in creating singleton instance");
+        }
+    }
+
+    public static StaticBlockSingleton getInstance() {
+        return instance;
+    }
+
+}
