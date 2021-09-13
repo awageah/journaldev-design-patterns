27faa45d95c97d5ffdf9aa025a727b85c77c4e5d
Ayman Wageah <aymn.wageah@gmail.com>
2021-09-13 15:18:46 +0200

    # implementing iterator Design Pattern

---
+++ Design Patterns/src/main/java/com/journaldev/behavioral/iterator/ChannelIterator.java
@@ -0,0 +1,9 @@
+package com.journaldev.behavioral.iterator;
+
+public interface ChannelIterator {
+
+    public boolean hasNext();
+
+    public Channel next();
+
+}
---
+++ Design Patterns/src/main/java/com/journaldev/behavioral/iterator/ChannelTypeEnum.java
@@ -0,0 +1,7 @@
+package com.journaldev.behavioral.iterator;
+
+public enum ChannelTypeEnum {
+
+    ENGLISH, HINDI, FRENCH, ALL;
+
+}
