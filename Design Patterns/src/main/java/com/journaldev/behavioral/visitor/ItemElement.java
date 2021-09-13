7fe5407dfe4aa7f19b8865166f5ec9671917add4
Ayman Wageah <aymn.wageah@gmail.com>
2021-09-13 14:50:04 +0200

    # implementing visitor Design Pattern

---
+++ Design Patterns/src/main/java/com/journaldev/behavioral/visitor/ItemElement.java
@@ -0,0 +1,7 @@
+package com.journaldev.behavioral.visitor;
+
+public interface ItemElement {
+
+    public int accept(ShoppingCartVisitor visitor);
+
+}
---
+++ Design Patterns/src/main/java/com/journaldev/behavioral/visitor/ShoppingCartVisitor.java
@@ -0,0 +1,9 @@
+package com.journaldev.behavioral.visitor;
+
+public interface ShoppingCartVisitor {
+
+    int visit(Book book);
+
+    int visit(Fruit fruit);
+
+}
